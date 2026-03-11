cc0e9235444c3ce784db9bf4dcbe8c3599106b5b
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-11 20:35:35 -0300

    Add files via upload

---
+++ Ler.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ler {
+    public static void main(String[] args) {
+       
+        Scanner leitor = new Scanner(System.in);
+
+        leitor.useDelimiter("[\r\n]+");
+
+        System.out.println("Entre com seu nome completo: ");
+        String nomeCompleto = leitor.next();
+        System.out.println(" Seu nome completo é : ");
+        System.out.println(nomeCompleto);
+
+    
+        leitor.close();
+
+    }
+}
